904b359d2ca348b8e5144189d2ee996245bb5d84
DESKTOP-77DPHAE\ROG <kimct567@gmail.com>
2021-03-26 09:14:23 +0900

    수정

--- Assets/Script/Wall.cs
+++ Assets/Script/Wall.cs
@@ -10,7 +10,7 @@ public class Wall : MonoBehaviour
     {
         transform.Translate(GameManager.Instance.speed *1f*Time.deltaTime, 0, 0);
         GameManager.Instance.speed -= Time.deltaTime;
-        if (transform.position.x < -10)
+        if (transform.position.x < -18)
         {
             GameManager.Instance.Score++;
             Destroy(gameObject);
